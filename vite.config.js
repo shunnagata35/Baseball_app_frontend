2b4cd7d20a920c6b4d457b3e0cf8dad9f8a1ca0e
Shun Nagata <shunthesheep@icloud.com>
2025-08-10 10:54:49 -0400

    new commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons/fa']
+      external: ['react-icons/fa','react-chartjs-2']
     }
   }
 })
